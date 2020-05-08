0108678a4cebfbc80fd781536cd8e0fe1f9cdcc7
Tito Olivares <ti.olivares@fisfor.cl>
2020-05-07 23:26:38 -0400

    database modified

--- core/admin.py
+++ core/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import TipoUsuario, Usuario
 
 # Register your models here.
-admin.site.register(Usuario)
 admin.site.register(TipoUsuario)
+admin.site.register(Usuario)
