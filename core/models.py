90b22adae0b5c5856a015ddc2b32ba8b30b2f4a7
Tito Olivares <ti.olivares@fisfor.cl>
2020-05-09 17:01:02 -0400

    details tables id added, settings.py timezone & language changed

--- core/models.py
+++ core/models.py
@@ -8,6 +8,72 @@
 from django.db import models
 
 
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128, blank=True, null=True)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150, blank=True, null=True)
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
+    email = models.CharField(max_length=254, blank=True, null=True)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
 class Boleta(models.Model):
     nro_boleta = models.AutoField(primary_key=True)
     total = models.FloatField()
@@ -20,9 +86,6 @@ class Boleta(models.Model):
         managed = False
         db_table = 'boleta'
 
-    def __str__(self):
-        return 'Boleta nro ' + self.nro_boleta
-
 
 class CatProducto(models.Model):
     id_categoria = models.SmallAutoField(primary_key=True)
@@ -31,14 +94,10 @@ class CatProducto(models.Model):
     class Meta:
         managed = False
         db_table = 'cat_producto'
-        verbose_name_plural = 'Categorías de productos'
-
-    def __str__(self):
-        return self.nombre
 
 
-# TODO: llenar al final cuando tenga datos para completarlos
 class DetalleBoleta(models.Model):
+    id_detalle_boleta = models.AutoField(primary_key=True)
     id_producto = models.ForeignKey('Producto', models.DO_NOTHING, db_column='id_producto')
     nro_boleta = models.ForeignKey(Boleta, models.DO_NOTHING, db_column='nro_boleta')
     cantidad = models.FloatField()
@@ -47,13 +106,10 @@ class DetalleBoleta(models.Model):
     class Meta:
         managed = False
         db_table = 'detalle_boleta'
-        verbose_name_plural = 'Detalles de boletas'
 
-    def __str__(self):
-        return 'Boleta nro ' + self.nro_boleta
 
-# TODO: llenar al final cuando tenga datos para completarlos
 class DetalleFactura(models.Model):
+    id_detalle_factura = models.AutoField(primary_key=True)
     nro_factura = models.ForeignKey('Factura', models.DO_NOTHING, db_column='nro_factura')
     id_producto = models.ForeignKey('Producto', models.DO_NOTHING, db_column='id_producto')
     cantidad = models.FloatField()
@@ -62,13 +118,10 @@ class DetalleFactura(models.Model):
     class Meta:
         managed = False
         db_table = 'detalle_factura'
-        verbose_name_plural = 'Detalles de facturas'
 
-    def __str__(self):
-        return 'Factura nro ' + self.nro_factura
 
-# TODO: llenar al final cuando tenga datos para completarlos
 class DetalleOrden(models.Model):
+    id_detalle_orden = models.AutoField(primary_key=True)
     id_producto = models.ForeignKey('Producto', models.DO_NOTHING, db_column='id_producto')
     id_orden = models.ForeignKey('OrdenCompra', models.DO_NOTHING, db_column='id_orden')
     cantidad = models.FloatField()
@@ -77,13 +130,10 @@ class DetalleOrden(models.Model):
     class Meta:
         managed = False
         db_table = 'detalle_orden'
-        verbose_name_plural = 'Detalles órdenes de compra'
 
-    def __str__(self):
-        return 'Orden nro ' + self.id_orden
 
-# TODO: llenar al final cuando tenga datos para completarlos
 class DetalleVenta(models.Model):
+    id_detalle_venta = models.AutoField(primary_key=True)
     id_venta = models.ForeignKey('Venta', models.DO_NOTHING, db_column='id_venta')
     id_producto = models.ForeignKey('Producto', models.DO_NOTHING, db_column='id_producto')
     cantidad = models.FloatField()
@@ -92,10 +142,50 @@ class DetalleVenta(models.Model):
     class Meta:
         managed = False
         db_table = 'detalle_venta'
-        verbose_name_plural = 'Detalles de ventas'
 
-    def __str__(self):
-        return 'Detalle de venta ' + self.id_venta
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200, blank=True, null=True)
+    action_flag = models.IntegerField()
+    change_message = models.TextField(blank=True, null=True)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100, blank=True, null=True)
+    model = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField(blank=True, null=True)
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
 
 
 class EstadoOrden(models.Model):
@@ -105,10 +195,6 @@ class EstadoOrden(models.Model):
     class Meta:
         managed = False
         db_table = 'estado_orden'
-        verbose_name_plural = 'Estados de órdenes'
-
-    def __str__(self):
-        return self.descripcion
 
 
 class EstadoVenta(models.Model):
@@ -118,10 +204,6 @@ class EstadoVenta(models.Model):
     class Meta:
         managed = False
         db_table = 'estado_venta'
-        verbose_name_plural = 'Estados de ventas'
-
-    def __str__(self):
-        return self.descripcion
 
 
 class Factura(models.Model):
@@ -142,9 +224,6 @@ class Factura(models.Model):
         managed = False
         db_table = 'factura'
 
-    def __str__(self):
-        return 'Factura nro ' + self.nro_factura
-
 
 class OrdenCompra(models.Model):
     id_orden = models.AutoField(primary_key=True)
@@ -156,10 +235,6 @@ class OrdenCompra(models.Model):
     class Meta:
         managed = False
         db_table = 'orden_compra'
-        verbose_name_plural = 'Órdenes de compras'
-
-    def __int__(self):
-        return 'Orden: ' + self.id_usuario
 
 
 class Perfil(models.Model):
@@ -169,10 +244,6 @@ class Perfil(models.Model):
     class Meta:
         managed = False
         db_table = 'perfil'
-        verbose_name_plural = 'Perfiles'
-
-    def __str__(self):
-        return self.nombre
 
 
 class Producto(models.Model):
@@ -190,9 +261,6 @@ class Producto(models.Model):
         managed = False
         db_table = 'producto'
 
-    def __str__(self):
-        return self.nombre
-
 
 class Recepcion(models.Model):
     id_recepcion = models.AutoField(primary_key=True)
@@ -204,11 +272,7 @@ class Recepcion(models.Model):
     class Meta:
         managed = False
         db_table = 'recepcion'
-        verbose_name_plural = "Recepciones"
         unique_together = (('id_recepcion', 'id_orden'),)
-
-    def __str__(self):
-        return 'Recepción orden ' + self.id_orden
 
 
 class Usuario(models.Model):
@@ -229,9 +293,6 @@ class Usuario(models.Model):
         managed = False
         db_table = 'usuario'
 
-    def __str__(self):
-        return self.nombre + ' ' + self.apellido
-
 
 class Venta(models.Model):
     id_venta = models.FloatField(primary_key=True)
@@ -242,6 +303,3 @@ class Venta(models.Model):
     class Meta:
         managed = False
         db_table = 'venta'
-
-    def __str__(self):
-        return self.id_venta
