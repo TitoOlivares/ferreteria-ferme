f2917a97d7986ecb38d452525339f3e9f532b103
Tito Olivares <ti.olivares@fisfor.cl>
2020-05-09 01:03:31 -0400

    model import on admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
-# from .models import TipoUsuario, Usuario
+from .models import Perfil, Usuario, Boleta, CatProducto, DetalleBoleta, DetalleFactura, DetalleOrden, DetalleVenta
+from .models import EstadoOrden, EstadoVenta, Factura, OrdenCompra, Producto, Recepcion, Venta
 
 # Register your models here.
+
+admin.site.register(Perfil)
+admin.site.register(Usuario)
+admin.site.register(Boleta)
+admin.site.register(CatProducto)
+admin.site.register(DetalleBoleta)
+admin.site.register(DetalleFactura)
+admin.site.register(DetalleOrden)
+admin.site.register(DetalleVenta)
+admin.site.register(EstadoOrden)
+admin.site.register(EstadoVenta)
+admin.site.register(Factura)
+admin.site.register(OrdenCompra)
+admin.site.register(Producto)
+admin.site.register(Recepcion)
+admin.site.register(Venta)
+
+
+
+
