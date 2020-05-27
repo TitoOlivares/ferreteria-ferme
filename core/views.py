0ca5c1911c2bf5eec60ea1c9979c1c707f063a47
Tito Olivares <ti.segovia@alumnos.duoc.cl>
2020-05-26 22:39:09 -0400

    login and register views

--- core/views.py
+++ core/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def home(request):
-    return render(request,'core/home.html')
+    return render(request, 'core/home.html')
+
+
+def login(request):
+    return render(request, 'core/login.html')
+
+
+def register(request):
+    return render(request, 'core/register.html')
+
