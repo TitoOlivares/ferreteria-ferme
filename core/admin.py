16b8af84def81ac250fa4e1be9e775cc04883b4c
Tito Olivares <ti.segovia@alumnos.duoc.cl>
2020-06-04 22:23:04 -0400

    admin panel and navbar modified

--- core/admin.py
+++ core/admin.py
@@ -1,21 +1,19 @@
 from django.contrib import admin
-from .models import Usuario, Boleta, CatProducto, DetalleBoleta, DetalleFactura, DetalleOrden, DetalleVenta
-from .models import EstadoOrden, EstadoVenta, Factura, OrdenCompra, Producto, Proveedor, Recepcion, Venta
-
+from .models import Usuario
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
-admin.site.register(Usuario)
-admin.site.register(Boleta)
-admin.site.register(CatProducto)
-admin.site.register(DetalleBoleta)
-admin.site.register(DetalleFactura)
-admin.site.register(DetalleOrden)
-admin.site.register(DetalleVenta)
-admin.site.register(EstadoOrden)
-admin.site.register(EstadoVenta)
-admin.site.register(Factura)
-admin.site.register(OrdenCompra)
-admin.site.register(Producto)
-admin.site.register(Proveedor)
-admin.site.register(Recepcion)
-admin.site.register(Venta)
+
+class UsuarioAdmin(UserAdmin):
+    list_display = ('email', 'nombre', 'apellido', 'rut')
+    search_fields = ('email', 'rut')
+    readonly_fields = ('last_login', 'date_joined')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+    ordering = ('email',)
+
+
+admin.site.register(Usuario, UsuarioAdmin)
